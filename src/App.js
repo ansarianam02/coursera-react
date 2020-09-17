8bb5f239f140d8d8e9e6a0ac2c5d8b173d466e77
ansarianam02 <ansari.anam02@gmail.com>
2020-09-17 17:01:53 +0530

    Components Part 1(no images)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./style.css";
+import Menu from './components/MenuComponent';
+
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="container">
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+      <Menu> </Menu>
     </div>
   );
 }
